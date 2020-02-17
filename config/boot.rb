ed983ae148b9a702462d008412fa55f449553751
Ori Pekelman <ori@pekelman.com>
2020-02-17 13:10:39 +0100

    update to latets upstream and add bootsnap config

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# For platform_sh
+Bootsnap.setup(cache_dir: "/tmp/cache")
